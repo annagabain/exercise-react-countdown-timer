378ebf5cc5ab0118c0f9cb067db999e8e175f4ba
AG <anna.gabain@gmail.com>
2024-08-28 16:05:12 +0200

    Pseudocode to structure the Countdown Timer functionality

--- src/CountdownTimer.tsx
+++ src/CountdownTimer.tsx
@@ -1,24 +1,33 @@
 import { useState, useRef, useEffect } from "react";
 
-export default function CountdownTimer () {
+export default function CountdownTimer() {
     console.log("hello from count donw timer!");
 
-    const  [timeLeft, setTimeLeft]= useState<number>(0)
-const  [isActive, setIsActive]= useState<boolean>(false)
+    const [timeLeft, setTimeLeft] = useState<number>(5);
+    const [isActive, setIsActive] = useState<boolean>(false);
+
+  const timeRef  = useRef<number>(null);
+
+    useEffect(() => {
+        console.log('hello from use effect');
+        isActive ? ( console.log('active')):( console.log('not active'));
+
+}, [isActive]);
 
     return (
         <div>
-          {/* TSX to be rendered */}
+              <h1>Nedräkningstimer</h1>
 
-           <p>Time left: {timeLeft}</p> 
-           <p>Is active:  {isActive}</p> 
-           <button onClick={() => setIsActive(!isActive)}>
-        {isActive ? "Pause" : "Start"}
-      </button>
+              <h2>{timeLeft} sekunder kvar</h2>
+            {/* <p>Is active:  {isActive}</p> */}
+            <button onClick={() => [setIsActive(!isActive), setTimeLeft(timeLeft => timeLeft - 1)] }>
+                {isActive ? "Pausa" : "Starta"}
+            </button>
+            <button onClick={() => console.log('resume')}>Återställ</button>
 
-           
+
         </div>
-      );
-    
+    );
+
 
 }
