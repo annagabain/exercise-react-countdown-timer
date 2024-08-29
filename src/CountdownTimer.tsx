6b8b7c357533a637103b2c3222cd126b589ce89f
AG <anna.gabain@gmail.com>
2024-08-29 09:55:05 +0200

    create the resume and pause functionalities

--- src/CountdownTimer.tsx
+++ src/CountdownTimer.tsx
@@ -1,22 +1,17 @@
 import { useState, useRef, useEffect } from "react";
 
 export default function CountdownTimer() {
-    // console.log("hello from count down timer!");
 
     const initialTime = 90
-
     const [timeLeft, setTimeLeft] = useState<number>(initialTime);
     const [isActive, setIsActive] = useState<boolean>(false);
-
     const timeRef = useRef<number | null>(null);
 
     useEffect(() => {
-        // console.log('hello from use effect');
 
-
-        isActive && timeLeft > 0 ? (  timeRef.current = window.setInterval(() => {
+        isActive && timeLeft > 0 ? (timeRef.current = window.setInterval(() => {
             setTimeLeft((previousTimeleft) => previousTimeleft - 1)
-        }, 1000)):( timeRef.current && clearInterval(timeRef.current));
+        }, 1000)) : (timeRef.current && clearInterval(timeRef.current));
 
 
         // if (isActive) {
@@ -39,8 +34,18 @@ export default function CountdownTimer() {
 
     }, [isActive, timeLeft]);
 
+    const handleStartandPause = () => {
+        // Reset the timer if it reached 0
+        // Toggle the active state
+        timeLeft === 0 ? setTimeLeft(initialTime) : setIsActive(!isActive)
+    }
+
+
     const handleReset = () => {
+        // Reset the timer if it reached 0
         setTimeLeft(initialTime);
+
+        // stop the timer running
         setIsActive(false);
     }
 
@@ -49,7 +54,7 @@ export default function CountdownTimer() {
             <h1>Nedräkningstimer</h1>
 
             <h2>{timeLeft} sekunder kvar</h2>
-            <button onClick={() => setIsActive(!isActive)}>
+            <button onClick={handleStartandPause}>
                 {isActive && timeLeft > 0 ? "Pausa" : "Starta"}
             </button>
             <button onClick={handleReset}>Återställ</button>
