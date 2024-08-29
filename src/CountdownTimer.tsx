e658bd9677e9bff3678b49f481861abaadac58e3
AG <anna.gabain@gmail.com>
2024-08-29 09:43:42 +0200

    Set intervals to the timer

--- src/CountdownTimer.tsx
+++ src/CountdownTimer.tsx
@@ -1,29 +1,58 @@
 import { useState, useRef, useEffect } from "react";
 
 export default function CountdownTimer() {
-    console.log("hello from count donw timer!");
+    // console.log("hello from count down timer!");
 
-    const [timeLeft, setTimeLeft] = useState<number>(5);
+    const initialTime = 90
+
+    const [timeLeft, setTimeLeft] = useState<number>(initialTime);
     const [isActive, setIsActive] = useState<boolean>(false);
 
-  const timeRef  = useRef<number>(null);
+    const timeRef = useRef<number | null>(null);
 
     useEffect(() => {
-        console.log('hello from use effect');
-        isActive ? ( console.log('active')):( console.log('not active'));
+        // console.log('hello from use effect');
 
-}, [isActive]);
+
+        isActive && timeLeft > 0 ? (  timeRef.current = window.setInterval(() => {
+            setTimeLeft((previousTimeleft) => previousTimeleft - 1)
+        }, 1000)):( timeRef.current && clearInterval(timeRef.current));
+
+
+        // if (isActive) {
+        //     timeRef.current = window.setInterval(() => {
+        //         setTimeLeft((previousTimeleft) => previousTimeleft - 1)
+        //     }, 1000);
+        // } else {
+        //     // Clear the interval if the timer is paused or if timeLeft reaches zero
+        //     if (timeRef.current) {
+        //         clearInterval(timeRef.current);
+        //     }
+        // }
+
+        return () => {
+            if (timeRef.current !== null) {
+                clearInterval(timeRef.current);
+                timeRef.current = null; // Reset the ref after clearing the interval
+            }
+        };
+
+    }, [isActive, timeLeft]);
+
+    const handleReset = () => {
+        setTimeLeft(initialTime);
+        setIsActive(false);
+    }
 
     return (
         <div>
-              <h1>Nedräkningstimer</h1>
+            <h1>Nedräkningstimer</h1>
 
-              <h2>{timeLeft} sekunder kvar</h2>
-            {/* <p>Is active:  {isActive}</p> */}
-            <button onClick={() => [setIsActive(!isActive), setTimeLeft(timeLeft => timeLeft - 1)] }>
-                {isActive ? "Pausa" : "Starta"}
+            <h2>{timeLeft} sekunder kvar</h2>
+            <button onClick={() => setIsActive(!isActive)}>
+                {isActive && timeLeft > 0 ? "Pausa" : "Starta"}
             </button>
-            <button onClick={() => console.log('resume')}>Återställ</button>
+            <button onClick={handleReset}>Återställ</button>
 
 
         </div>
